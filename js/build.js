d6889218334f515f48b160c4c78779e10c554155
hcarneiro <hcarneiro@weboo.biz>
2018-09-12 14:30:32 +0100

    wip - changes to use Fliplet.Registry

--- js/build.js
+++ js/build.js
@@ -1,10 +1,10 @@
 var dynamicLists = dynamicLists || {};
-$('[data-dynamic-lists-id]').each(function(){
-  var container = this;
-  var id = $(this).data('dynamic-lists-id');
-  var data = Fliplet.Widget.getData(id);
 
-  if (!data.layout) {
-    dynamicLists[id] = new DynamicLists(data, container);
-  }
+Fliplet.Widget.instance('dynamic-lists-1-2-0', function (data) {
+  var container = this;
+  var id = $(this).data('dynamic-lists-1-2-0-id');
+  var layoutType = !data.layout ? 'general' : data.layout;
+  var DynamicList = Fliplet.Registry.get('dynamic-list:1.2.0:' + layoutType);
+
+  dynamicLists[id] = new DynamicList(id, data, container);
 });
