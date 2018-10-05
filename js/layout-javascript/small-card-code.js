aa314d486de99acfc96e9e8f0fdc819bd98d4e31
hcarneiro <hcarneiro@weboo.biz>
2018-10-05 10:36:34 +0100

    Fix filtering

--- js/layout-javascript/small-card-code.js
+++ js/layout-javascript/small-card-code.js
@@ -1104,8 +1104,8 @@ DynamicList.prototype.filterList = function() {
   var _this = this;
   _this.filterClasses = [];
 
+  var listData = _this.searchedListItems ? _this.searchedListItems : _this.listItems;
   if (!$('.hidden-filter-controls-filter.mixitup-control-active').length) {
-    var listData = _this.searchedListItems ? _this.searchedListItems : _this.listItems;
     _this.prepareToRenderLoop(listData);
     _this.renderLoopHTML();
     _this.onReady();
@@ -1116,7 +1116,7 @@ DynamicList.prototype.filterList = function() {
     _this.filterClasses.push($(element).data('toggle'));
   });
 
-  var filteredData = _.filter(_this.listItems, function(row) {
+  var filteredData = _.filter(listData, function(row) {
     var filters = [];
     row.data['flFilters'].forEach(function(obj) {
       filters.push(obj.data.class);
