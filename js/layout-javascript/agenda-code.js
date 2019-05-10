7f2269583123243328ac73b9a0425476d4e63779
Tony Wu <twu@fliplet.com>
2019-05-10 13:42:52 +0100

    Normalize data value formats when grouping agenda items

--- js/layout-javascript/agenda-code.js
+++ js/layout-javascript/agenda-code.js
@@ -1100,7 +1100,8 @@ DynamicList.prototype.groupLoopDataByDate = function (loopData, dateField) {
   var _this = this;
   // Group data by date field
   var recordGroups = _.groupBy(loopData, function(row) {
-    return row[dateField];
+    // Format date value as it could be in various formats
+    return _this.Utils.Date.moment(row[dateField]).format('YYYY-MM-DD');
   });
   var recordMerges = [];
 
