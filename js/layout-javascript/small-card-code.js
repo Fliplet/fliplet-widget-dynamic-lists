325a7348761c9f6d77dbbc13234defa12f9fde12
Tony Wu <twu@fliplet.com>
2019-06-20 15:53:29 +0100

    Adds "openSingleEntry" support in query parameters

--- js/layout-javascript/small-card-code.js
+++ js/layout-javascript/small-card-code.js
@@ -1453,16 +1453,6 @@ DynamicList.prototype.overrideSearchData = function(value) {
   var copyOfValue = value;
   value = value.toLowerCase();
 
-  $inputField.val('');
-  $inputField.blur();
-  _this.$container.find('.hidden-search-controls').addClass('is-searching').removeClass('no-results');
-  _this.$container.find('.hidden-search-controls').addClass('active');
-
-  // Removes cards
-  _this.$container.find('#small-card-wrapper-' + _this.data.id).html('');
-  // Adds search query to HTML
-  _this.$container.find('.current-query').html(value);
-
   // Search
   var searchedData = [];
   var filteredData;
@@ -1482,13 +1472,31 @@ DynamicList.prototype.overrideSearchData = function(value) {
     });
   } else {
     searchedData = _.filter(_this.listItems, function(obj) {
-      return _this.Utils.Record.contains(obj.data[field], value);
+      return _this.Utils.Record.contains(obj, value);
     });
 
     if (!searchedData || !searchedData.length) {
       searchedData = [];
     }
   }
+
+  // Remove duplicates
+  searchedData = _.uniq(searchedData);
+
+  if (_this.pvSearchQuery && _this.pvSearchQuery.openSingleEntry && searchedData.length === 1) {
+    _this.showDetails(searchedData[0].id);
+    return;
+  }
+
+  $inputField.val('');
+  $inputField.blur();
+  _this.$container.find('.hidden-search-controls').addClass('is-searching').removeClass('no-results');
+  _this.$container.find('.hidden-search-controls').addClass('active');
+
+  // Removes cards
+  _this.$container.find('#small-card-wrapper-' + _this.data.id).html('');
+  // Adds search query to HTML
+  _this.$container.find('.current-query').html(value);
 
   _this.$container.find('.hidden-search-controls').removeClass('is-searching no-results').addClass('search-results');
   _this.$container.find('.new-small-card-list-container').removeClass('searching');
@@ -1507,14 +1515,7 @@ DynamicList.prototype.overrideSearchData = function(value) {
     $('.limit-entries-text').addClass('hidden');
   }
 
-  // Remove duplicates
-  searchedData = _.uniq(searchedData);
   _this.searchedListItems = searchedData;
-
-  if (_this.pvSearchQuery && _this.pvSearchQuery.openSingleEntry && _this.searchedListItems.length === 1) {
-    _this.showDetails(_this.searchedListItems[0].id);
-  }
-
   _this.prepareToRenderLoop(searchedData);
   _this.renderLoopHTML(function(from, to){
     _this.onPartialRender(from, to);
@@ -1600,10 +1601,6 @@ DynamicList.prototype.searchData = function(value) {
     // Remove duplicates
     searchedData = _.uniq(searchedData);
     _this.searchedListItems = searchedData;
-
-    if (_this.querySearch && _this.pvSearchQuery && _this.pvSearchQuery.openSingleEntry && _this.searchedListItems.length === 1) {
-      _this.showDetails(_this.searchedListItems[0].id);
-    }
 
     _this.prepareToRenderLoop(searchedData);
     _this.renderLoopHTML(function(from, to){
