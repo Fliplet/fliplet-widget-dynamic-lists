a5af7145a71d76f403d8d61cb248a9f7a003c829
Tony Wu <twu@fliplet.com>
2019-08-06 09:04:56 +0100

    Typo

--- js/utils.js
+++ js/utils.js
@@ -374,7 +374,7 @@ Fliplet.Registry.set('dynamicListUtils', (function () {
 
     return _.filter(records, function (record) {
       return _.every(filters, function (filter) {
-        if (!filter.condition === 'none' || filter.column === 'none' || !filter.value) {
+        if (filter.condition === 'none' || filter.column === 'none' || !filter.value) {
           // Filter isn't configured correctly
           return true;
         }
