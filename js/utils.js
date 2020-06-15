3add7d4c64d4288dc2407e7f379257a109de66f8
Maksym Shokin <maksym.sh@upplabs.com>
2020-06-15 18:17:12 +0300

    fixes

--- js/utils.js
+++ js/utils.js
@@ -394,28 +394,33 @@ Fliplet.Registry.set('dynamicListUtils', (function () {
 
     return _.filter(records, function (record) {
       return _.every(filters, function (filter) {
-        if (filter.condition === 'empty') {
-          return _.isEmpty(record.data[filter.column]);
-        }
-          
-        if (filter.condition === 'notempty') {
-          return !_.isEmpty(record.data[filter.column]); 
-        }
+        var condition = filter.condition;
+        var rowData = _.get(record, 'data.' + filter.column, null);
 
-        if (filter.condition === 'none' || filter.column === 'none' || !filter.value) {
+        if (condition === 'none' || filter.column === 'none') {
           // Filter isn't configured correctly
           return true;
         }
 
-        var condition = filter.condition;
-        var rowData;
+        if (condition === 'empty') {
+          return _.isEmpty(rowData) && !_.isFinite(rowData);
+        }
+
+        if (condition === 'notempty') {
+          return !_.isEmpty(rowData) && !_.isFinite(rowData);
+        }
+
+        if (!filter.value) {
+          // Value is not configured
+          return true;
+        }
 
         // Case insensitive
         if (typeof filter.value === 'string') {
           filter.value = filter.value.toLowerCase();
         }
 
-        if (!_.isNull(_.get(record, 'data.' + filter.column, null))) {
+        if (!_.isNull(rowData)) {
           rowData = record.data[filter.column].toString().toLowerCase();
         }
 
