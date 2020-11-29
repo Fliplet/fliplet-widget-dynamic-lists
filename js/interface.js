332d3cd6a5838e9d799eef53a83ab0047f5258bb
Andrey Riazantsev <71251809+AndrRyaz@users.noreply.github.com>
2020-11-29 22:57:07 +0200

    eslint fix

--- js/interface.js
+++ js/interface.js
@@ -1202,6 +1202,7 @@ var DynamicLists = (function() {
       }
       if (context === 'relations') {
         $('.relations-tab').removeClass('present').addClass('future');
+
         initDataSourceProvider(_this.config.dataSourceId);
       }
       if (context === 'layouts') {
