dd54fdc69ed1dc1a73874ba3b4b3764e348152e8
AndryRyaz <andriy.r@upplabs.com>
2020-12-01 14:24:14 +0200

    Update filter list

--- js/interface.js
+++ js/interface.js
@@ -1795,7 +1795,7 @@ var DynamicLists = (function() {
     },
     addFilterItem: function(data) {
       data.columnLabel = data.column === 'none'
-        ? '(Field)'
+        ? ''
         : data.column;
       data.logicLabel = logicMap[data.logic]
         ? logicMap[data.logic]
