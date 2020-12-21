662979b572cd0b6a35dd11f53bc0ab843e7e38ee
ivandevupp <ivan.s@upplabs.com>
2020-12-21 12:55:47 +0200

    lint fix

--- js/interface.js
+++ js/interface.js
@@ -709,21 +709,23 @@ var DynamicLists = (function() {
       };
 
       for (var type in options) {
-        var accessRuleIndex = -1;
-
-        accessRules.forEach(function(item, index) {
-          var typeIndex = item.type.indexOf(type);
-
-          if (typeIndex !== -1) {
-            accessRuleIndex = index;
+        if (options.hasOwnProperty(type)) {
+          var accessRuleIndex = -1;
+  
+          accessRules.forEach(function(item, index) {
+            var typeIndex = item.type.indexOf(type);
+  
+            if (typeIndex !== -1) {
+              accessRuleIndex = index;
+            }
+          });
+  
+          if (options[type] && accessRuleIndex === -1) {
+            defaultRule.type = [type];
+            accessRules.push(defaultRule);
+          } else if (!options[type] && accessRuleIndex > -1) {
+            accessRules.splice(accessRuleIndex, 1);
           }
-        });
-
-        if (options[type] && accessRuleIndex === -1) {
-          defaultRule.type = [type];
-          accessRules.push(defaultRule);
-        } else if (!options[type] && accessRuleIndex > -1) {
-          accessRules.splice(accessRuleIndex, 1);
         }
       }
     },
