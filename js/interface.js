51ff4584fd38f86f3dd8894dccbf24a289888677
Andrey Riazantsev <71251809+AndrRyaz@users.noreply.github.com>
2021-01-25 15:57:56 +0200

    Filter value type: Enter a value

--- js/interface.js
+++ js/interface.js
@@ -224,6 +224,7 @@ var DynamicLists = (function() {
           item.logic = 'none';
           item.valueType = 'enter-value';
           item.value = '';
+          item.valueField = 'Value';
           item.columns = dataSourceColumns;
           _this.config.filterOptions.push(item);
 
@@ -234,7 +235,6 @@ var DynamicLists = (function() {
           var value = $(this).val();
           var type = $(this).data('field');
           var $selector = $(this).parents('.filter-panel');
-          var id = $(this).attr('filter-item-id');
 
           if (type === 'field') {
             $selector.find('.panel-title-text .column').html(value === 'none' ? '(Field)' : value);
@@ -245,10 +245,6 @@ var DynamicLists = (function() {
 
             $selector.find('.panel-title-text .value, #value-dash, #filter-value-type').toggleClass('hidden', hideValueFields);
             $selector.find('.panel-title-text .value, #value-dash, #filter-value').toggleClass('hidden', hideValueFields);
-          }
-
-          if (type === 'valueType') {
-            $selector.find('#filter-value label').html(value !== 'enter-value' ? 'Value for' : 'Value');
           }
 
           if (type === 'valueType') {
@@ -1923,6 +1919,9 @@ var DynamicLists = (function() {
       data.columnLabel = data.column === 'none'
         ? '(Field)'
         : data.column;
+      data.valueField = data.valueType === 'enter-value'
+        ? 'Value'
+        : 'Value for';
 
       var $newPanel = $(filterPanelTemplate(data));
 
