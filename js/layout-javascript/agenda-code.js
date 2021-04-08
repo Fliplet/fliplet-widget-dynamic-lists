990701ab00b8d8f916306bae529d4ef6abbbec59
ivandevupp <ivan.s@upplabs.com>
2021-04-08 19:18:40 +0300

    fix hiding before beforeOpen hook is resolved

--- js/layout-javascript/agenda-code.js
+++ js/layout-javascript/agenda-code.js
@@ -559,8 +559,6 @@ DynamicList.prototype.attachObservers = function() {
         return;
       }
 
-      _this.$container.find('.new-agenda-list-container, .dynamic-list-add-item').addClass('hidden');
-
       var entryId = $(this).data('entry-id');
       var entryTitle = $(this).find('.agenda-item-title').text().trim();
       var beforeOpen = Promise.resolve();
@@ -580,6 +578,8 @@ DynamicList.prototype.attachObservers = function() {
       }
 
       beforeOpen.then(function() {
+        _this.$container.find('.new-agenda-list-container, .dynamic-list-add-item').addClass('hidden');
+
         Fliplet.Analytics.trackEvent({
           category: 'list_dynamic_' + _this.data.layout,
           action: 'entry_open',
