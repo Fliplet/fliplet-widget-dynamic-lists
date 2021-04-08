a0972a89d37ad2049302fa6bcee69c84f27da17f
Andrey Riazantsev <71251809+AndrRyaz@users.noreply.github.com>
2021-04-08 14:59:34 +0300

    Fix/od 476 (#13)

--- js/layout-javascript/agenda-code.js
+++ js/layout-javascript/agenda-code.js
@@ -1739,6 +1739,7 @@ DynamicList.prototype.animateDateForward = function(nextDateElement, nextDateEle
 // animates cards forward
 DynamicList.prototype.animateAgendaForward = function(nextAgendaElement, nextAgendaElementWidth) {
   var _this = this;
+  var $dayHolders = _this.$container.find('.agenda-list-day-holder');
 
   return new Promise(function(resolve) {
     _this.$container.find('.agenda-cards-wrapper').animate({
@@ -1747,8 +1748,9 @@ DynamicList.prototype.animateAgendaForward = function(nextAgendaElement, nextAge
     _this.ANIMATION_SPEED,
     'swing',  // animation easing
     function() {
-      _this.$container.find('.agenda-list-day-holder.active').removeClass('active').addClass('hidden');
+      $dayHolders.removeClass('active').not(nextAgendaElement).addClass('hidden');
       nextAgendaElement.addClass('active');
+
       _this.scrollValue = $(this).scrollLeft();
       _this.copyOfScrollValue = _this.scrollValue;
 
@@ -1778,6 +1780,7 @@ DynamicList.prototype.animateDateBack = function(prevDateElement, prevDateElemen
 // animate cards back
 DynamicList.prototype.animateAgendaBack = function(prevAgendaElement, prevAgendaElementWidth) {
   var _this = this;
+  var $dayHolders = _this.$container.find('.agenda-list-day-holder');
 
   return new Promise(function(resolve) {
     _this.$container.find('.agenda-cards-wrapper').animate({
@@ -1786,8 +1789,9 @@ DynamicList.prototype.animateAgendaBack = function(prevAgendaElement, prevAgenda
     _this.ANIMATION_SPEED,
     'swing',  // animation easing
     function() {
-      _this.$container.find('.agenda-list-day-holder.active').removeClass('active').addClass('hidden');
+      $dayHolders.removeClass('active').not(prevAgendaElement).addClass('hidden');
       prevAgendaElement.addClass('active');
+
       _this.scrollValue = $(this).scrollLeft();
       _this.copyOfScrollValue = _this.scrollValue;
       resolve();
