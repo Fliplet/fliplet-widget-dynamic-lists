29fe2f597f99d1e9cd6e26c76e0de01986dd9625
skrupnyk <serhii.kr@upplabs.com>
2021-06-29 12:55:12 +0300

    fix email title and data-chat-email

--- js/layout-javascript/small-h-card-code.js
+++ js/layout-javascript/small-h-card-code.js
@@ -634,6 +634,7 @@ DynamicList.prototype.addSummaryData = function(records) {
     var newObject = {
       id: entry.id,
       chatEnabled: entry.chatEnabled,
+      chatEmail: entry.chatEmail,
       editEntry: entry.editEntry,
       deleteEntry: entry.deleteEntry,
       isCurrentUser: entry.isCurrentUser ? entry.isCurrentUser : false,
