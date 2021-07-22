e1c4a573fc207ca124aea85e788ae40ea968e708
OleksandrLavrynenko <86256215+OleksandrLavrynenko@users.noreply.github.com>
2021-07-22 17:03:17 +0300

    OD-382 [Feature] Use locale format for displaying dates and times (#592)
    * [ADD] add local date
    
    * [ADD] refact
    
    * [ADD] add local date for comments
    
    * [FIX] fixed locale date to comments
    
    * [FIX] fix longDateFormat
    
    * [FIX] fix date in comments
    
    * [FIX] fix commit
    
    * Revert "[FIX] fix commit"
    
    This reverts commit 851da5b7683ffef4b2fef9dbaa43873d4778bfa1.
    
    * [FIX] fix commit

--- js/interface.js
+++ js/interface.js
@@ -62,6 +62,10 @@ var DynamicLists = (function() {
   var defaultSettings = window.flListLayoutConfig;
   var defaultColumns = window.flListLayoutTableColumnConfig;
   var defaultEntries = window.flListLayoutTableConfig;
+  var LOCALE_FORMATS = {
+    TIME: moment.localeData().longDateFormat('LT'),
+    DATE: moment.localeData().longDateFormat('ll')
+  };
 
   // Constructor
   function DynamicLists(configuration) {
@@ -2002,15 +2006,15 @@ var DynamicLists = (function() {
       }
     },
     addSummaryItem: function(data) {
-      data.date = moment().format('MMM Do YYYY');
-      data.time = moment().format('h:mm A');
+      data.date = moment().format(LOCALE_FORMATS.DATE);
+      data.time = moment().format(LOCALE_FORMATS.TIME);
 
       var $newPanel = $(summaryRowTemplate(data));
 
       $summaryRowContainer.append($newPanel);
     },
     addDetailItem: function(data) {
-      data.date = moment().format('MMM Do YYYY');
+      data.date = moment().format(LOCALE_FORMATS.DATE);
 
       var $newPanel = $(detailsRowTemplate(data));
 
