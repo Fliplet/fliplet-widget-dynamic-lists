213c9a6cbb4c529afe3389fb344907ccc412f00f
OleksandrLavrynenko <oleksandr.lav@upplabs.com>
2021-10-15 10:57:41 +0300

    [FIX] fix sort

--- js/layout-javascript/simple-list-code.js
+++ js/layout-javascript/simple-list-code.js
@@ -54,7 +54,7 @@ function DynamicList(id, data) {
   this.pvPreFilterQuery;
   this.pvOpenQuery;
   this.openedEntryOnQuery = false;
-  this.sortOrder = 'asc';
+  this.sortOrder = 'none';
   this.sortField = null;
   this.imagesData = {};
 
@@ -214,8 +214,20 @@ DynamicList.prototype.attachObservers = function() {
         asc: 'fa-sort-asc',
         desc: 'fa-sort-desc'
       };
+      var currentSortOrder = $sortListItem.data('sortOrder');
 
-      _this.sortOrder = $sortListItem.data('sortOrder') === 'asc' ? 'desc' : 'asc';
+      switch (currentSortOrder) {
+        case 'asc':
+          _this.sortOrder = 'desc';
+          break;
+        case 'desc':
+          _this.sortOrder = 'none';
+          break;
+        default:
+          _this.sortOrder = 'asc';
+          break;
+      }
+
       _this.sortField = $sortListItem.data('sortField');
       _this.Utils.DOM.resetSortIcons({ $sortList: $sortList });
 
