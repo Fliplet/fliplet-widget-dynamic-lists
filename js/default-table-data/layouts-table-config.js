776f9ed3455a23aa920363a55f356c958b0cbb81
OleksandrLavrynenko <oleksandr.lav@upplabs.com>
2021-11-12 16:48:47 +0200

    [ADD] add local date to news feed

--- js/default-table-data/layouts-table-config.js
+++ js/default-table-data/layouts-table-config.js
@@ -52,21 +52,21 @@ window.flListLayoutTableConfig = {
       'Title': 'This year’s most popular business apps',
       'Image': 'https://cdn.fliplet.com/apps/2659/de1ac7174a201cff518f13b38a293d30363-001-3021.jpg',
       'Categories': 'Enterprise software',
-      'Date': moment().subtract(3, 'months').format('YYYY-MM-DD'),
+      'Date': moment().locale('en').subtract(3, 'months').format('YYYY-MM-DD'),
       'Content': '<p>Most enteprises now agree that their bottom line could benefit from bespoke apps.  But which ones are they building and where do they think the real value lies?</p><p>Fliplet works with many of the world’s industry leading businesses. We’ve crunched the data to identify the trends in the world of enterprise app building.  In this report we’ll share some of the findings and talk through a few case studies that are at the cutting edge</p><h3>Revenue generation or cost savings</h3><p>Technology in the workplace is often thought of as a way to streamline process, reducing friction and ultimately creating cost savings.  However it is actually the revenue generating functions that are making the widest use of apps.  Sales and marketing teams are using apps to reach out to new clients and engage more with existing ones.  They tend to be amongst the most prolific app builders - producing an a new app every couple of months</p><p>To learn more about enterprise app trends - visit <a href=“https://fliplet.com”>Fliplet.com</a></p>'
     },
     {
       'Title': 'Artificial intelligence in the workplace',
       'Image': 'https://cdn.fliplet.com/apps/2659/7b98218ffd06b23fd5b99bfaf53bbd7c194-850-1800.jpg',
       'Categories': 'Machine Learning',
-      'Date': moment().subtract(1, 'week').format('YYYY-MM-DD'),
+      'Date': moment().locale('en').subtract(1, 'week').format('YYYY-MM-DD'),
       'Content': '<p>Venture capitalists are investing heavily in AI; China has made dominance in AI a national goal; Elon Musk and Stephen Hawking have warned us about letting AI escape from our grip...  So why haven’t we seen more of it in our every day lives...</p><p>The short answer is that we have - it’s just that AI is often invisible; working in the background; making existing processes 5-10% more efficient rather than inventing entirely new processes.  This is particularly true in the areas of recommendation engines, big data analysis and healthcare diagnosis</p><p>The areas where AI has had the greatest media coverage have been impersonating humans, chess playing and of course as evil overlords of humans. This is a vast overstatement of AIs current powers.  It might be something to be wary of in the future but current manifestations of AI fall somewhat short of the media hype</p>'
     },
     {
       'Title': 'Why do business apps lag so far behind consumer apps?',
       'Image': 'https://cdn.fliplet.com/apps/2659/5f292d9d7898a4c654ad87c90a40f66c156-875-9870.jpg',
       'Categories': 'Apps',
-      'Date': moment().subtract(1, 'months').subtract(3, 'days').format('YYYY-MM-DD'),
+      'Date': moment().locale('en').subtract(1, 'months').subtract(3, 'days').format('YYYY-MM-DD'),
       'Content': '<p>When I started my first job I was amazed at the cutting edge technology my employer had lavished on me. Not one but two flat screens greeted me. No clunky cathode ray tubes in site. Swipe card passes granted me swift access to a lift that arrived as I walked up to it. And of course the icing on the cake - a shiny new Blackberry</p><p>But somewhere along the way business technology brands got left behind.  Consumer brands surged forward. Apple shot passed Blackberry and Microsoft. Google and Amazon - two consumer brands at their core - made huge inroads into enterprise software and server architecture. An average consumer app today makes its corporate equivalent look prehistoric.</p>'
     }
   ],
